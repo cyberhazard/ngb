fcee652695915f29c2d896baa44e3398b153c094
cyber <cyber>
2017-10-18 00:21:33 +0300

    feat: products hover script

--- src/js/main.js
+++ src/js/main.js
@@ -1,6 +1,5 @@
 /**
  * Закрепленное к верху меню навигации
- *
  */
 const fixedNavigationMenu = () => {
   const navigation = document.querySelector('.navigation');
@@ -13,6 +12,25 @@ const fixedNavigationMenu = () => {
       navigation.style.paddingTop = '';
     }
   }
+};
+
+/**
+ * Блок с выбором продуктов. Отображение активных продуктов при наведении.
+ * Весь контент находится в папке vendor, файл products.json
+ */
+const productsBlock = () => {
+  const title = document.querySelector('.products__title');
+  const text = document.querySelector('.products__text');
+  const circles = [...document.querySelectorAll('.products .circle')]
+  fetch('/vendor/products.json').then(r => r.json()).then(products => {
+    circles.forEach((c, i) => {
+      c.onmouseover = () => {
+        title.innerHTML = products[i].title;
+        text.innerHTML = products[i].text;
+      }
+    })
+  })
 }
 
-fixedNavigationMenu()
+fixedNavigationMenu();
+productsBlock();
