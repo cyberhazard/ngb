75957ca4552ca7532810e0e52dc557fe68b98500
Zolotukhin Anatoliy <zolotukhinwebpr@mail.ru>
2017-10-18 18:09:39 +0700

    modal script

--- src/js/main.js
+++ src/js/main.js
@@ -15,6 +15,71 @@ const fixedNavigationMenu = () => {
 };
 
 /**
+ * Модальные окна для заказа обратного звонка
+ */
+
+/**
+ * Инициализация
+ */
+// var scrollW = window.innerWidth - document.body.clientWidth;
+var modal = new tingle.modal({
+  stickyFooter: false,
+  closeMethods: ['overlay', 'button', 'escape'],
+  closeLabel: "Close",
+  beforeClose: function() {
+    document.querySelector('body').style.overflow= 'auto';
+    document.querySelector('body').style.paddingRight= 0 + 'px';
+    return true; // close the modal
+  },
+  cssClass: ['custom-class-1', 'custom-class-2']
+});
+
+/**
+ * Форма обратного звонка
+ */
+var callBackWrap = () => {
+  return`<div class="call">
+            <div class="g-wrapper">
+              <div class="call__content">
+                <div class="call__title"> Заказать звонок</div>
+                <form class="call__form">
+                  <div class="call__item">
+                    <label class="call__field"> * Ваше имя:</label>
+                    <input class="call__input" type="text" required name="name" placeholder="Например, Антон" />
+                  </div>
+                  <div class="call__item">
+                    <label class="call__field"> * Ваш телефон:</label>
+                    <input class="call__input" type="tel" required name="phone" placeholder="+7 (999) 999-99-99" />
+                  </div>
+                  <button class="button call__submit"> Записаться </button>
+                </form>
+              </div>
+            </div>
+          </div>
+        `
+};
+
+
+/**
+ * Функция вызова модальных окон
+ */
+var callBack = function(){
+  const callBackButton = Array.prototype.slice.call(document.querySelectorAll('.callback'));
+  if(!callBackButton) return null;
+  callBackButton.forEach((el) => el.onclick = function(e){
+    e.preventDefault();
+    modal.setContent(callBackWrap());
+    document.querySelector('body').style.paddingRight= window.innerWidth - document.body.clientWidth + 'px';
+    document.querySelector('body').style.overflow= 'hidden';
+    document.querySelector('.tingle-modal').style.background="rgba(0, 0, 0, .9)";
+    document.querySelector('.tingle-modal').style.zIndex="1000000";
+    document.querySelector('.tingle-modal-box').style.background="none";
+    modal.open();
+  })
+}
+callBack();
+
+/**
  * Блок с выбором продуктов. Отображение активных продуктов при наведении.
  * Весь контент находится в папке vendor, файл products.json
  */
