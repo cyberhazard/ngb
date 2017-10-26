14aaec7234e296fe1659d39a01561770bc6a8989
Zolotukhin Anatoliy <zolotukhinwebpr@mail.ru>
2017-10-27 04:11:23 +0700

    ebaniy mail php

--- src/mail.php
+++ src/mail.php
@@ -1,15 +1,16 @@
-<? echo $_POST["user_name"]; ?>
-<? echo $_POST["user_phone"]; ?>
+<? echo $_POST["user-name"]; ?>
+<? echo $_POST["user-phone"]; ?>
 <? echo $_POST; ?>
 
 
 <?php
-$to      = 'zolotukhinwebpr@mail.ru';
+$to      = 'neftegazbroker@neftegazbroker.ru';
 $subject = 'Заявка с сайта';
-$message = 'Заявка.  Имя: ' . $_POST["user-name"] . ' Телефон: ' . $_POST["user-phone"] . ' Сообщение: ' . $_POST["user-text"];
-$headers = 'From: admin@legpos-auto.ru' . "\r\n" .
+$mmm = isset($_POST["user-text"])? ' Сообщение: ' . $_POST["user-text"] : '';
+$message = 'Заявка.  Имя: ' . $_POST["user-name"] . ' Телефон: ' . $_POST["user-phone"] . $mmm;
+$headers = 'From: admin@beget.tech' . "\r\n" .
     'Content-Type: text/plain; charset=utf8;' . "\r\n" .
-    'Reply-To: admin@ngb.ru' . "\r\n" .
+    'Reply-To: admin@beget.tech' . "\r\n" .
     'X-Mailer: PHP/' . phpversion();
 mail($to, $subject, $message, $headers);
 ?>
